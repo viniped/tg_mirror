90c27d1f2daf2cbfc7d84041ca6ddc0d6781d3ec
vinicius-pedro <125911962+viniped@users.noreply.github.com>
2023-08-13 16:26:09 -0300

    Update tg_mirror.py
    Adicionado barra de progresso
     
    Extração de thumbnail
    
    Registro de log de tarefas no formato downloaded_midia_{channel_source}_{channel_target}.json permitindo retomada de tarefas para uma tarefa executada anteriormente
    
    Registo no log somente do ID da ultima midia processada permitindo retomada rapida a partir da proxima mensagem

--- tg_mirror.py
+++ tg_mirror.py
@@ -4,164 +4,195 @@ import json
 from pyrogram import Client
 from pathlib import Path
 import subprocess
-from utils import show_banner, cache_path, authenticate
-
-def get_channel_title(client, channel_id_or_username):
-    channel = client.get_chat(channel_id_or_username)
-    return channel.title
-
-def create_channel(client, title):
-    channel = client.create_channel(title=title)
-    return channel.id  
+from tqdm import tqdm
+from utils import Banner, show_banner, cache_path, authenticate
 
 """ Global """
-session_name = "user"        
+session_name = "user"
+video_path = 'downloads'
 
 def get_channels():
     with Client(session_name) as client:
         channel_source = input("Forneça o @username ou ID do canal / grupo de origem: ")
         channel_target = input("Forneça o @username ou ID do canal de destino: ")
-
-        # Se o canal de origem foi especificado e o de destino foi deixado em branco
-        if channel_source and not channel_target:
-            source_title = get_channel_title(client, channel_source)
-            channel_target_name = f"{source_title} - Cópia"
-            channel_target = create_channel(client, channel_target_name)
-
-    return channel_source, channel_target     
+        channel_source = parse_channel_input(channel_source)
+        channel_target = parse_channel_input(channel_target)
+        
+    return  channel_source, channel_target
 
 def parse_channel_input(channel_input: str):
     """Parse channel input to determine if it's an ID or username."""
     if channel_input.startswith("@"):
-        return channel_input  
+        return channel_input
     else:
         try:
-            return int(channel_input)  
+            return int(channel_input)
         except ValueError:
             print("Entrada inválida. Por favor, forneça um ID ou nome de usuário válido.")
             exit()
 
 def get_user_choices():
-    print("Quais conteudos você deseja processar ?:\n")
-    options = ["Processar todos os Conteúdos" , "Fotos","Áudios", "Vídeos", "Arquivos", "Texto", "Sticker", "Animação - GIFs"]
+    print("Quais conteudos você deseja processar?:\n")
+    options = ["Processar todos os Conteúdos", "Fotos", "Áudios", "Vídeos", "Arquivos", "Texto", "Sticker", "Animação - GIFs"]
     for i, option in enumerate(options):
         print(f"{i} - {option}")
-    choices = input("\nInforme os conteúdos que deseja procesar separados por vírgula (ex: 1,3) < 0 para processar todos > : ").split(',')
+    choices = input("\nInforme os conteúdos que deseja procesar separados por vírgula (ex: 1,3) < 0 para processar todos : ").split(',')
     choices = [int(choice.strip()) for choice in choices]
-    if 0 in choices:  # Se o usuário escolher a opção 0
-        choices = [1, 2, 3, 4, 5, 6, 7] 
-    return choices            
+    if 0 in choices:
+        choices = [1, 2, 3, 4, 5, 6, 7]
+    return choices
 
-def collect_video_metadata(directory: str) -> dict:
-    videos_metadata = {}
-    for video_file in os.listdir(directory):
-        if video_file.endswith(('.mp4', '.mkv', '.avi')):  # você pode adicionar mais extensões conforme necessário
-            video_path = os.path.join(directory, video_file)
-            ffprobe_command = f"ffprobe -v quiet -print_format json -show_format -show_streams {video_path}"
-            output = os.popen(ffprobe_command).read().strip()
+def extract_thumbnail(video_path: str) -> str:
+    thumbnail_path = video_path + ".jpg"
 
-            if output:
-                try:
-                    metadata = json.loads(output)
-                    videos_metadata[video_file] = metadata
-                except json.JSONDecodeError:
-                    pass
+    # Extract frame from 00:00:01
+    thumbnail_command = [
+        'ffmpeg',
+        '-i', video_path,
+        '-ss', '00:00:01',
+        '-vframes', '1',
+        thumbnail_path
+    ]
+    try:
+        subprocess.run(thumbnail_command)
+        return thumbnail_path
+    except Exception as e:
+        print(f"Erro ao extrair miniatura: {e}")
+        return ""
 
-    return videos_metadata
+def collect_video_duration(video_path: str) -> int:
+    try:
+        ffprobe_command = [
+            'ffprobe',
+            '-v', 'error',
+            '-show_entries', 'format=duration',
+            '-of', 'default=noprint_wrappers=1:nokey=1',
+            video_path
+        ]
+        duration = subprocess.check_output(ffprobe_command).decode('utf-8').strip()
+        return int(float(duration))
+    except Exception as e:
+        print(f"Erro ao coletar duração do vídeo: {e}")
+        return 0
+
+def get_json_filename(channel_source, channel_target):
+    return f"downloaded_media_{channel_source}_{channel_target}.json"
 
 def download_and_upload_media_from_channel(choices, channel_source, channel_target):
     downloaded_media = []
+    last_processed_id = 0
+    json_filename = get_json_filename( channel_source, channel_target)
 
-    # Load task progress from Json file, if exists
-    if os.path.exists("downloaded_media.json"):
-        with open("downloaded_media.json", "r") as json_file:
-            downloaded_media = json.load(json_file)
-            print("Progresso anterior carregado do arquivo JSON.")
-
-    downloaded_ids = [item["id"] for item in downloaded_media]
+    if os.path.exists(json_filename):
+        with open(json_filename, "r") as json_file:
+            data = json.load(json_file)
+            last_processed_id = data["last_processed_id"] if "last_processed_id" in data else 0
+            print(f"Retomando do ID da próxima mensagem após a última processada: {last_processed_id + 1}")
 
     with Client(session_name) as client:
-         
         all_messages = list(client.get_chat_history(channel_source))
         all_messages.reverse()
-        # Get 'duration' argument of videos before upload
-        all_video_metadata = collect_video_metadata('downloads')
-        
-        for count, message in enumerate(all_messages):
-            # Verify if media was downloaded before
-            if message.id in downloaded_ids:
-                os.system('clear || cls')
-                print(f"Mensagem {message.id} já foi baixada anteriormente.")
-                continue
+        start_processing = False
 
+        for count, message in enumerate(all_messages):
+            
+            if start_processing or message.id > last_processed_id:
+                start_processing = True
+            else:
+                continue
             file_name = None
             caption_text = message.caption
+            duration = 0
+            download_start_time = None
+            last_update_time = None
+            bytes_downloaded = 0
+         
+            def progress(current, total, operation="Downloading"):
+                nonlocal download_start_time, last_update_time, bytes_downloaded
+                if download_start_time is None:
+                    download_start_time = time.time()
+                    last_update_time = download_start_time
+                else:
+                    current_time = time.time()
+                    elapsed_time = current_time - last_update_time
+                    
+                    if elapsed_time > 0.5:
+                        speed_bps = (current - bytes_downloaded) / elapsed_time  # bytes por segundo
+                        speed_mbps = (speed_bps * 8) / (10**6)  # megabits por segundo
 
-            # Process and download content 
-      
+                        bar.set_description(f"{operation} at {speed_mbps:.2f} Mbps")
+                        bytes_downloaded = current
+                        last_update_time = current_time
+                bar.n = current
+                bar.refresh()
+           
             if 1 in choices and message.photo:
-                file_name = client.download_media(message.photo)
-                client.send_photo(channel_target, file_name, caption=caption_text)
+                file_size = message.photo.file_size
+                bar = tqdm(total=file_size, desc="Downloading", leave=False)
+                file_name = client.download_media(message.photo, progress=progress)
+                client.send_photo(channel_target, file_name, caption=caption_text, progress=progress)        
+           
             if 2 in choices and message.audio:
-                file_name = client.download_media(message.audio)
-                client.send_audio(channel_target, file_name, caption=caption_text)
+                #os.system('clear || cls')
+                file_size = message.audio.file_size
+                bar = tqdm(total=file_size, desc="Downloading", leave=False)                         
+                #file_name = client.download_media(message.audio, progress=progress)
+                file_name = client.download_media(message.audio, progress=lambda c, t: progress(c, t, "Downloading"))
+                file_size = os.path.getsize(file_name)
+                bar = tqdm(total=file_size, desc="Uploading ...", leave=False)
+                #client.send_audio(channel_target, file_name, caption=caption_text, progress=progress)
+                client.send_audio(channel_target, file_name, caption=caption_text, progress=lambda c, t: progress(c, t, "Uploading"))
+  
             if 3 in choices and message.video:
-                file_name = client.download_media(message.video)
-                client.send_video(channel_target, file_name, caption=caption_text)
+                #os.system('clear || cls')
+                file_size = message.video.file_size
+                bar = tqdm(total=file_size, desc="Downloading",unit = 'B', leave=False)
+                #file_name = client.download_media(message.video, progress=progress)
+                file_name = client.download_media(message.video, progress=lambda c, t: progress(c, t, "Downloading"))
+                duration = collect_video_duration(file_name)
+                thumbnail_path = extract_thumbnail(file_name)
+                
+                if thumbnail_path:
+                    file_size = os.path.getsize(file_name)
+                    bar = tqdm(total=file_size, desc="Uploading ...", leave=False)
+                    #client.send_video(channel_target, file_name, caption=caption_text, duration=duration, thumb=thumbnail_path, progress=progress)
+                    client.send_video(channel_target, file_name, caption=caption_text, duration=duration, thumb=thumbnail_path, progress=lambda c, t: progress(c, t, "Uploading"))
+
+                    os.remove(thumbnail_path)  # Remove thumbnail file after uploads
+                else:
+                    file_size = os.path.getsize(file_name)
+                    bar = tqdm(total=file_size, desc="Uploading ...", unit='B', leave=False)
+                    client.send_video(channel_target, file_name, caption=caption_text, duration=duration, progress=progress)
+               
             if 4 in choices and message.document:
-                file_name = client.download_media(message.document)
-                client.send_document(channel_target, file_name, caption=caption_text)
-            if 5 in choices and message.text:
+                #os.system('clear || cls')
+                file_size = message.document.file_size
+                bar = tqdm(total=file_size, desc="Downloading", leave=False)
+                file_name = client.download_media(message.document, progress=progress)
+                client.send_document(channel_target, file_name, caption=caption_text, progress=progress)
+                
+            if 5 in choices and message.text:                
                 client.send_message(channel_target, message.text)
+                
             if 6 in choices and message.sticker:
                 file_name = client.download_media(message.sticker)
                 client.send_sticker(channel_target, file_name)
             if 7 in choices and message.animation:
                 file_name = client.download_media(message.animation)
-                client.send_animation(channel_target, file_name) 
-                
-                # Obter o argumento duração para os videos baixados antes do upload
-                if file_name:
-                    video_metadata = all_video_metadata.get(os.path.basename(file_name), {})
-                    duration = video_metadata.get("format", {}).get("duration", "")
+                client.send_animation(channel_target, file_name)
 
-            # Depois de baixar e fazer o upload, adicione detalhes ao JSON
             if file_name:
-                media_type = None
-                if message.photo:
-                    media_type = "photo"#1
-                elif message.audio:
-                    media_type = "audio"#2
-                elif message.video:
-                    media_type = "video"#3
-                elif message.document:
-                    media_type = "document"#4
-                elif message.text:
-                    media_type = "text"#5
-                elif message.sticker:
-                    media_type = "sticker"#6
-                elif message.animation:
-                    media_type = "animation"#7    
-    
-                downloaded_media.append({
-                    "id": message.id,
-                    "type": media_type,
-                    "file_name": file_name
-                })
+                last_processed_id = message.id
+                with open(json_filename, "w") as json_file:
+                    json.dump({"last_processed_id": last_processed_id}, json_file)
+                    os.system('clear || cls')
+                    print(f"Detalhes da mensagem {message.id} adicionados à lista e mídia / arquivo enviada ao canal de destino.")
+              
+                # Remova o arquivo baixado após o upload
+                os.remove(file_name)  
 
-                # Salvar detalhes da mídia baixada em um arquivo JSON após o upload
-                with open("downloaded_media.json", "w") as json_file:
-                    json.dump(downloaded_media, json_file, indent=4)
-               
-                # Remover arquivo baixado após o upload e atualizar o JSON
-                os.remove(file_name)
-                os.system('clear || cls')
-                print(f"Detalhes da mensagem {message.id} adicionados à lista e mídia / arquivo enviada ao canal de destino.")
-                
-
-            # Intervalo de 10s para evitar abuso da API do Telegram
-            time.sleep(10)
-
+            # Intervalo de 5s para evitar abuso da API do Telegram
+            time.sleep(5)
         print("Tarefa concluida e log salvo no arquivo JSON.")
 
 if __name__ == "__main__":
@@ -171,4 +202,3 @@ if __name__ == "__main__":
     channel_source, channel_target = get_channels()
     choices = get_user_choices()
     download_and_upload_media_from_channel(choices, channel_source, channel_target)
-
